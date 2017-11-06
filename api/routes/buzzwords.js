f0531c3bb0e2c098c28489a5cbd7b07077ee30e1
Jeff Montgomery <Jeff.Montgomery@omaticsoftware.local>
2017-11-06 15:46:06 -0500

    almost completed stage-2

--- api/routes/buzzwords.js
+++ api/routes/buzzwords.js
@@ -4,7 +4,7 @@ const buzzwords = require('buzzwords');
 const {map, addIndex} = require('ramda')
 const mapIndexed = addIndex(map);
 const createDoc = (v, i) => ({id: i, name: v, value: v})
-const docs = mapIndexed(createDoc, y)
+const docs = mapIndexed(createDoc, buzzwords)
 
 
 module.exports = app => {
